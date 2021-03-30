cc2ac17a713bb425f4dc9c8806685b7752ca60e1
Mirna Gama <mirnabeatrizgamav@gmail.com>
2021-03-29 21:50:52 -0300

    Tela Principal (Home), Toolbar e Footer

--- frontend/gestao-pedidos/src/app/app.module.ts
+++ frontend/gestao-pedidos/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
