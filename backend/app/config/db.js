90479d54a9d6fd27dee340d3b72a84e50575eb8a
julio.oliveira <julio.oliveira@philips.com>
2021-04-18 02:07:21 -0300

    fix: change host name in db.js

--- backend/app/config/db.js
+++ backend/app/config/db.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const sequelizeConnect = new Sequelize('the_ranch_db', 'root', 'root', {dialect: 'mysql', host: 'mysql'});
+const sequelizeConnect = new Sequelize('the_ranch_db', 'root', 'root', {dialect: 'mysql', host: 'mysql-ranch'});
 //const sequelizeConnect = new Sequelize('heroku_7aa488ddd03433f', 'ba5b023c05b33c', 'f2485e4c', {dialect: 'mysql', host: 'us-cdbr-east-03.cleardb.com'});
 module.exports = {sequelizeConnect, Sequelize};
