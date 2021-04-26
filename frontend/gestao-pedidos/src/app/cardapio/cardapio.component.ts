92f5b80ca34069907092afe4c9ebfde5efc586db
EigoNC <igofacul@gmail.com>
2021-04-26 08:51:41 -0300

    [ Gestão de Pedidos ] - Correção no menu dropdown de Cardápio

--- frontend/gestao-pedidos/src/app/cardapio/cardapio.component.ts
+++ frontend/gestao-pedidos/src/app/cardapio/cardapio.component.ts
@@ -1,22 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { windowCount } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cardapio',
   templateUrl: './cardapio.component.html',
   styleUrls: ['./cardapio.component.css']
 })
-export class CardapioComponent implements OnInit {
+export class CardapioComponent {
 
   item: string;
-  
-  constructor(private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-
-    // TO-DO: MUDAR A SEÇÃO EM TEMPO REAL (NO MOMENTO SO FUNCIONA SE DER REFRESH)
-    this.item = this.route.snapshot.params['item'];
-
-  }
+  constructor(public route: ActivatedRoute) { }
 
 }
